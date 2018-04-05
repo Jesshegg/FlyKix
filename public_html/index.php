785387375d00f1a9f97a69756cdd22c48d13afef
Jesshegg <35411937+Jesshegg@users.noreply.github.com>
2018-04-05 14:16:29 -0500

    Homepage

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,30 @@
 <?php
 require dirname(__DIR__) ."/bootstrap.php";
-echo "hello world"
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <title>Flykix</title>
+   <meta charset=:"utf-8">
+   <meta name="viewport" content="width=device-width,initial-scale=1">
+   <a href="index.css"
+ </head>
+ <body>
+
+   <div class="header">
+     <h1>Flykix</h1>
+   </div>
+
+   <div class="navigation">
+     <a href="index.css">Womens</a>
+     <a href="index.css">Mens</a>
+   </div>
+
+   <div class="row">
+     <div class="column side">
+
+   <div class="footer">
+
+ </body>
+ </html>
