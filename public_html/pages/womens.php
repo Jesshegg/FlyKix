e1641a18442a1680a4cea3aded5e06224a0ce3cf
Nate Nolting <me@natenolting.com>
2018-04-12 14:30:04 -0500

    add to cart code

--- public_html/pages/womens.php
+++ public_html/pages/womens.php
@@ -1,12 +1,28 @@
-<div class="entry">
-    <img src="images/apples-colors-fashion-185476.jpg" height="500" width="400"/>
-</div>
+<?php 
+    $data = [];
+    array_push($data, [
+        'category' => 'womens',
+        'id' => 'sdkfjsdfksf',
+        'cost' => '10',
+        'image' => 'images/apples-colors-fashion-185476.jpg'
+    ])
 
-{
-  "id":
-  "name":
-  "cost":
-  "description":
-  "image":
-  "category":
-}
+?>
+
+<div class="entry">
+    <img src="images/apples-colors-fashion-185476.jpg"/>
+    <?php 
+        for($i=0; $i < count($data); $i++;) {
+            echo '<img src="'. $data[$i]['image'] .'" />';
+            $form = null;
+            $form .= '<form action="/path/to/cart.php" method="post">';
+            $form .= '<input type="hidden" name="product-id" value="'. $data[$i]['id'] .'" />';
+            //$form .= '<input type="text" name="product-quantity" plaeholder="How many?" />';
+            $form .= '<input type="submit" value="Add To Cart" name="submit" />';
+            $form .= '</form>'
+            
+            echo $form;
+        }
+    
+    ?>
+</div>
