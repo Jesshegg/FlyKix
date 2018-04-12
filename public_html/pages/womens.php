57674c7708784a205c329d1a2645368ccd2d32a9
Jesshegg <35411937+Jesshegg@users.noreply.github.com>
2018-04-12 15:16:15 -0500

    added images

--- public_html/pages/womens.php
+++ public_html/pages/womens.php
@@ -1,6 +1,12 @@
 <div class="entry">
     <img src="images/apples-colors-fashion-185476.jpg" height="500" width="400"/>
 </div>
+<div class="entry">
+    <img src="images/healthy-light-woman-legs.jpg" height="325" width="400"/>
+</div>
+<div class="entry">
+    <img src="images/pexels-photo-112285.jpg" height="325" width="400"/>
+</div>
 
 {
   "id":
